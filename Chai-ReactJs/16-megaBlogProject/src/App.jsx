806266adef0e227bbb69ff94b3e62b18e0d2a4a8
umarfaisal295c <umarfaisalumarfaisal447@gmail.com>
2024-01-05 22:59:45 +0500

    add redux toolkit in this mega project

--- Chai-ReactJs/16-megaBlogProject/src/App.jsx
+++ Chai-ReactJs/16-megaBlogProject/src/App.jsx
@@ -1,11 +1,37 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appWrite/auth.js";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index.js";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>hy</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  // conditional rendering.
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Test
+          {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
